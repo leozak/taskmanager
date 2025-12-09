8dd8a323b6d3e294097f785e54dc85f8d597b5eb
Leonardo Reiczak <leozak@gmail.com>
2025-12-09 14:09:17 -0300

    Implementação inicial do login.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Login from "./components/Login/Login";
 import Schedule from "./components/Schedule/Schedule";
 import SideBar from "./components/Sidebar/Sidebar";
 
@@ -13,32 +14,41 @@ function App() {
 
   const [todos, setTodos] = useState([]);
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   useEffect(() => {
     setTodos(api("/todos"));
   }, []);
 
   return (
     <>
-      <div className="flex min-h-full">
-        <SideBar
-          nowMonth={month}
-          nowYear={year}
-          nowDay={day}
-          setMonth={setMonth}
-          setYear={setYear}
-          setDay={setDay}
-        />
+      <div className="flex h-screen">
+        {/* Login */}
+        {!isLoggedIn ? (
+          <Login setIsLoggedIn={setIsLoggedIn} />
+        ) : (
+          <>
+            <SideBar
+              nowMonth={month}
+              nowYear={year}
+              nowDay={day}
+              setMonth={setMonth}
+              setYear={setYear}
+              setDay={setDay}
+            />
 
-        <Schedule
-          nowMonth={month}
-          nowYear={year}
-          nowDay={day}
-          setMonth={setMonth}
-          setYear={setYear}
-          setDay={setDay}
-          todos={todos}
-          setTodos={setTodos}
-        />
+            <Schedule
+              nowMonth={month}
+              nowYear={year}
+              nowDay={day}
+              setMonth={setMonth}
+              setYear={setYear}
+              setDay={setDay}
+              todos={todos}
+              setTodos={setTodos}
+            />
+          </>
+        )}
       </div>
     </>
   );
